cc2c7e90d3fe6888d5a372832e4ceda461ab6b0b
Akhil Pillai <ackledotdev@gmail.com>
2025-09-23 12:29:01 -0700

    Update redirect URLs in redirects.ts

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -1,3 +1,4 @@
 export const Redirects: Record<string, string> = {
-	"litcritquiz": "https://github.com/ackledotdev/literary-criticism-quiz",
+	"litcritquiz": "https://github.com/ackledotdev/literary-criticism-quiz/",
+	"": "https://developer.mozilla.org/"
 }
