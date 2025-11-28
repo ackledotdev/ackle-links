401e4a3016755319cb2d0af731f44a6d7402f3ac
Akhil Pillai <ackledotdev@gmail.com>
2025-11-27 22:21:21 -0800

    Add pronouns.page redirects

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -1,4 +1,6 @@
 export const Redirects: Record<string, string> = {
 	"litcritquiz": "https://github.com/ackledotdev/literary-criticism-quiz/",
-	"": "https://developer.mozilla.org/"
+	"": "https://developer.mozilla.org/",
+	"pronouns": "https://en.pronouns.page/@equus_quagga",
+	"pnpage": "https://en.pronouns.page/@equus_quagga"
 }
